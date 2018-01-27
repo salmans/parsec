965d32234aeac08c4161a44f4a7c77a2239e3b9b
Salman <salman.saghafi@me.com>
2018-01-27 10:04:00 -0800

    combine combinators into the same file

--- src/main/java/combinator/Combinator.kt
+++ src/main/java/combinator/Combinator.kt
@@ -1,7 +1,56 @@
 package combinator
 
 import parser.Parser
+import parser.plus
 import tools.Either
+
+fun <T, R> many(parser: Parser<T, R>) = { tokens: Sequence<T> ->
+    var result = emptyList<R>()
+    var toks = tokens
+
+    while (true) {
+        val parsed = parser(toks)
+        if (parsed is Either.Left) {
+            break
+        } else if (parsed is Either.Right) {
+            val (r, ts) = parsed.value
+            toks = ts
+            result += r
+        }
+    }
+
+    Either.right(result to toks)
+}
+
+fun <T, R> optional(parser: Parser<T, R>) = { tokens: Sequence<T> ->
+    parser(tokens).let {
+        when (it) {
+            is Either.Left -> Either.right(null to tokens)
+            is Either.Right -> it
+        }
+    }
+}
+
+infix fun <T, F, S> Parser<T, F>.or(parser: Parser<T, S>) = { tokens: Sequence<T> ->
+    val firstResult = this(tokens)
+    when (firstResult) {
+        is Either.Left -> {
+            val secondResult = parser(tokens)
+            when(secondResult) {
+                is Either.Left -> Either.left(firstResult.value + secondResult.value)
+                is Either.Right -> {
+                    val (sr, sts) = secondResult.value
+                    Either.right(Either.right(sr) to sts)
+                }
+            }
+
+        }
+        is Either.Right -> {
+            val (fr, fts) = firstResult.value
+            Either.right(Either.left(fr) to fts)
+        }
+    }
+}
 
 infix fun <T, F, S> Parser<T, F>.and(parser: Parser<T, S>): Parser<T, Pair<F, S>> = { tokens: Sequence<T> ->
     val firstResult = this(tokens)
